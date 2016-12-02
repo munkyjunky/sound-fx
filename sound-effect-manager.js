08aa8b487bf00f739bd6068959ea6c3269ee819a
Wez Pyke <wez.pyke@gmail.com>
2016-12-02 11:17:55 +0000

    Added support for base64 encoded strings

--- sound-effect-manager.js
+++ sound-effect-manager.js
@@ -1,11 +1,11 @@
 /*
-SoundEffectManager
+ SoundEffectManager
 
-Loads and plays sound effects useing
-HTML5 Web Audio API (as only available in webkit, at the moment).
+ Loads and plays sound effects useing
+ HTML5 Web Audio API (as only available in webkit, at the moment).
 
-By @HenrikJoreteg from &yet
-*/
+ By @HenrikJoreteg from &yet
+ */
 
 // Get the audio extension to use for the current browser
 function getAudioExtension () {
@@ -19,183 +19,212 @@ function getAudioExtension () {
 }
 
 function SoundEffectManager () {
-    this.AudioContext = window.AudioContext || window.webkitAudioContext;
+	this.AudioContext = window.AudioContext || window.webkitAudioContext;
 
-    this.support = !!this.AudioContext;
-    if (this.support) {
-        this.context = new this.AudioContext();
-    }
+	this.support = !!this.AudioContext;
+	if (this.support) {
+		this.context = new this.AudioContext();
+	}
 
-    this.sounds = {};
-    this.sources = {};
+	this.sounds = {};
+	this.sources = {};
 }
 
 // async load a file at a given URL, store it as 'name'.
 SoundEffectManager.prototype.loadFile = function (url, name, delay, cb) {
-    if (this.support) {
-        this._loadWebAudioFile(url, name, delay, cb);
-    } else {
-        this._loadFallbackAudioFile(url.replace('.mp3', getAudioExtension()), name, delay, 3, cb);
-    }
+	if (this.support) {
+		this._loadWebAudioFile(url, name, delay, cb);
+	} else {
+		this._loadFallbackAudioFile(url.replace('.mp3', getAudioExtension()), name, delay, 3, cb);
+	}
 };
 
 // async load a file at a given URL, store it as 'name'.
 SoundEffectManager.prototype._loadWebAudioFile = function (url, name, delay, cb) {
-    if (!this.support) {
-        return;
-    }
+	if (!this.support) {
+		return;
+	}
 
-    var self = this;
-    var request = new XMLHttpRequest();
+	var self = this;
+	var request = new XMLHttpRequest();
 
-    request.open('GET', url, true);
-    request.responseType = 'arraybuffer';
-    request.onload = function () {
-        self.context.decodeAudioData(request.response,
-            function (data) { // Success
-                self.sounds[name] = data;
-                if (cb) {
-                    cb(null, data);
-                }
-            },
-            function (err) { // Error
-                if (cb) {
-                    cb(err);
-                }
-            }
-        );
-    };
+	if (url.indexOf('data') !== -1) {
+		self.sounds[name] = url;
+		return cb(url);
+	}
 
-    setTimeout(function () {
-        request.send();
-    }, delay || 0);
+	request.open('GET', url, true);
+	request.responseType = 'arraybuffer';
+	request.onload = function () {
+		self.context.decodeAudioData(request.response,
+			function (data) { // Success
+				self.sounds[name] = data;
+				if (cb) {
+					cb(null, data);
+				}
+			},
+			function (err) { // Error
+				if (cb) {
+					cb(err);
+				}
+			}
+		);
+	};
+
+	setTimeout(function () {
+		request.send();
+	}, delay || 0);
 };
 
 SoundEffectManager.prototype._loadFallbackAudioFile = function (url, name, delay, multiplexLimit, cb) {
-    var self = this;
-    var limit = multiplexLimit || 3;
+	var self = this;
+	var limit = multiplexLimit || 3;
 
-    setTimeout(function () {
-        var a, i = 0;
+	setTimeout(function () {
+		var a, i = 0;
 
-        self.sounds[name] = [];
-        while (i < limit) {
-            a = new Audio();
-            a.src = url;
-            // for our callback
-            if (i === 0 && cb) {
+		self.sounds[name] = [];
+		while (i < limit) {
+			a = new Audio();
+			a.src = url;
+			// for our callback
+			if (i === 0 && cb) {
 
-            	a.addEventListener('canplaythrough', function () {
-                	cb(null, a);
-                }, false);
+				a.addEventListener('canplaythrough', function () {
+					cb(null, a);
+				}, false);
 
-	            a.addEventListener('error', function (e) {
+				a.addEventListener('error', function (e) {
 
-	            	var err;
+					var err;
 
-		            switch (e.target.error.code) {
-			            case e.target.error.MEDIA_ERR_SRC_NOT_SUPPORTED:
-			            	err = new Error('MEDIA_ERR_SRC_NOT_SUPPORTED');
-				            break;
-			            case e.target.error.MEDIA_ERR_ABORTED:
-				            err = new Error('MEDIA_ERR_ABORTED');
-				            break;
-			            case e.target.error.MEDIA_ERR_DECODE:
-				            err = new Error('MEDIA_ERR_DECODE');
-				            break;
-			            case e.target.error.MEDIA_ERR_NETWORK:
-				            err = new Error('MEDIA_ERR_NETWORK');
-				            break;
-			            default:
-			            	err = new Error();
-				            break;
-		            }
+					switch (e.target.error.code) {
+						case e.target.error.MEDIA_ERR_SRC_NOT_SUPPORTED:
+							err = new Error('MEDIA_ERR_SRC_NOT_SUPPORTED');
+							break;
+						case e.target.error.MEDIA_ERR_ABORTED:
+							err = new Error('MEDIA_ERR_ABORTED');
+							break;
+						case e.target.error.MEDIA_ERR_DECODE:
+							err = new Error('MEDIA_ERR_DECODE');
+							break;
+						case e.target.error.MEDIA_ERR_NETWORK:
+							err = new Error('MEDIA_ERR_NETWORK');
+							break;
+						default:
+							err = new Error();
+							break;
+					}
 
-	            	cb(err);
-	            }, false);
+					cb(err);
+				}, false);
 
-            }
-            a.load();
-            self.sounds[name][i++] = a;
-        }
-    }, delay || 0);
+			}
+			a.load();
+			self.sounds[name][i++] = a;
+		}
+	}, delay || 0);
 };
 
 SoundEffectManager.prototype._playWebAudio = function (soundName, loop) {
-    var buffer = this.sounds[soundName];
+    var self = this;
+	var buffer = this.sounds[soundName];
 
-    if (!buffer) {
-        return;
-    }
+	if (!buffer) {
+		return;
+	}
 
-    if (loop && this.sources[soundName]) {
-        // Only start the sound once if it's looping
-        return;
-    }
+	if (loop && this.sources[soundName]) {
+		// Only start the sound once if it's looping
+		return;
+	}
 
-    var source = this.context.createBufferSource();
-    source.buffer = buffer;
-    source.loop = loop;
-    source.connect(this.context.destination);
+	function base64ToArrayBuffer(base64) {
+		var binaryString = window.atob(base64);
+		var len = binaryString.length;
+		var bytes = new Uint8Array(len);
+		for (var i = 0; i < len; i++)        {
+			bytes[i] = binaryString.charCodeAt(i);
+		}
+		return bytes.buffer;
+	}
 
-    if (loop) {
-        this.sources[soundName] = source;
-    }
 
-    source.start(0);
+	// Base64 data string
+	if (buffer.indexOf('data') !== -1) {
+		const newBuffer = buffer.replace('data:audio/mpeg;base64,', '');
+
+		var source = self.context.createBufferSource();
+
+		this.context.decodeAudioData(base64ToArrayBuffer(newBuffer), function(decodedData) {
+			source.buffer = decodedData;
+			source.connect(self.context.destination);
+			source.start(0);
+		});
+	} else {
+		source.buffer = buffer;
+		source.loop = loop;
+		source.connect(self.context.destination);
+
+		if (loop) {
+			this.sources[soundName] = source;
+		}
+
+		source.start(0);
+	}
 };
 
 SoundEffectManager.prototype._playFallbackAudio = function (soundName, loop) {
-    var audio = this.sounds[soundName];
-    var howMany = audio && audio.length || 0;
-    var i = 0;
-    var currSound;
+	var audio = this.sounds[soundName];
+	var howMany = audio && audio.length || 0;
+	var i = 0;
+	var currSound;
 
-    if (!audio) {
-        return;
-    }
+	if (!audio) {
+		return;
+	}
 
-    while (i < howMany) {
-        currSound = audio[i++];
-        // this covers case where we loaded an unplayable file type
-        if (currSound.error) {
-            return;
-        }
-        if (currSound.currentTime === 0 || currSound.currentTime === currSound.duration) {
-            currSound.currentTime = 0;
-            currSound.loop = !!loop;
-            i = howMany;
-            return currSound.play();
-        }
-    }
+	while (i < howMany) {
+		currSound = audio[i++];
+		// this covers case where we loaded an unplayable file type
+		if (currSound.error) {
+			return;
+		}
+		if (currSound.currentTime === 0 || currSound.currentTime === currSound.duration) {
+			currSound.currentTime = 0;
+			currSound.loop = !!loop;
+			i = howMany;
+			return currSound.play();
+		}
+	}
 };
 
 SoundEffectManager.prototype.play = function (soundName, loop) {
-    if (this.support) {
-        this._playWebAudio(soundName, loop);
-    } else {
-        return this._playFallbackAudio(soundName, loop);
-    }
+	if (this.support) {
+		this._playWebAudio(soundName, loop);
+	} else {
+		return this._playFallbackAudio(soundName, loop);
+	}
 };
 
 SoundEffectManager.prototype.stop = function (soundName) {
-    if (this.support) {
-        if (this.sources[soundName]) {
-            this.sources[soundName].stop(0);
-            delete this.sources[soundName];
-        }
-    } else {
-        var soundArray = this.sounds[soundName];
-        var howMany = soundArray && soundArray.length || 0;
-        var i = 0;
-        var currSound;
+	if (this.support) {
+		if (this.sources[soundName]) {
+			this.sources[soundName].stop(0);
+			delete this.sources[soundName];
+		}
+	} else {
+		var soundArray = this.sounds[soundName];
+		var howMany = soundArray && soundArray.length || 0;
+		var i = 0;
+		var currSound;
 
-        while (i < howMany) {
-            currSound = soundArray[i++];
-            currSound.pause();
-            currSound.currentTime = 0;
-        }
-    }
+		while (i < howMany) {
+			currSound = soundArray[i++];
+			currSound.pause();
+			currSound.currentTime = 0;
+		}
+	}
 };
 
 
